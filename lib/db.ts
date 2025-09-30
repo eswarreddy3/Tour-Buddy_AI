f034876d2fcfd51d6c677148f3d6933db6de648a
Venkata Sai Eswar Reddy Pagadala <109944287+eswarreddy3@users.noreply.github.com>
2025-09-30 23:15:26 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -5,10 +5,10 @@ let pool: mysql.Pool
 export function getDB() {
   if (!pool) {
     pool = mysql.createPool({
-      host: process.env.DB_HOST || "localhost",
-      user: process.env.DB_USER || "root",
-      password: process.env.DB_PASS || "",
-      database: process.env.DB_NAME || "tourbuddyai",  //tourismdata tourbuddyai
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,  //tourismdata tourbuddyai
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0,
